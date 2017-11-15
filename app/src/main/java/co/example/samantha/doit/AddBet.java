9de404d93939573fcdac37e4e8d39d57a9aab79d
saml0616 <samlee@brandeis.edu>
2017-11-15 01:27:27 -0500

    Initial commit

---
+++ app/src/main/java/co/example/samantha/doit/AddBet.java
@@ -0,0 +1,25 @@
+package co.example.samantha.doit;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class AddBet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_bet);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
