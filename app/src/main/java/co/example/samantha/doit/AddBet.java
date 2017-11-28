9c3830a8d8555d4ca80e5104218e2d30cf8603fc
gavinyahna <31702834+gavinyahna@users.noreply.github.com>
2017-11-28 14:35:50 -0500

    added creation of calender event

--- app/src/main/java/co/example/samantha/doit/AddBet.java
+++ app/src/main/java/co/example/samantha/doit/AddBet.java
@@ -2,15 +2,17 @@ package co.example.samantha.doit;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.provider.CalendarContract;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.util.Date;
+import java.util.List;
 
 public class AddBet extends AppCompatActivity {
 
@@ -38,20 +40,44 @@ public class AddBet extends AppCompatActivity {
 
         EditText betName = (EditText) findViewById(R.id.betname);
         EditText location = (EditText) findViewById(R.id.locationInput);
-        EditText descripfield = (EditText) findViewById(R.id.descripinput);
+        EditText description = (EditText) findViewById(R.id.descripinput);
         EditText endDate = (EditText) findViewById(R.id.dueDateInput);
         EditText wager = (EditText) findViewById(R.id.wagerinput);
+
+        //creates calender event
+        Intent calenderIntent = new Intent(Intent.ACTION_INSERT_OR_EDIT);
+        calenderIntent.setType("vnd.android.cursor.item/event");
+        calenderIntent.putExtra(CalendarContract.Events.TITLE, betName.getText().toString());
+        calenderIntent.putExtra(CalendarContract.Events.DESCRIPTION, description.getText().toString());
+        calenderIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, location.getText().toString());
+        //startActivity(calenderIntent);
+
+        // Verify it resolves
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(calenderIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        // Start an activity if it's safe
+        if (isIntentSafe) {
+            startActivity(calenderIntent);
+        } else {
+            Toast.makeText(this, "You're calender app is not set up correctly.", Toast.LENGTH_SHORT).show();
+        }
+
+        //Returns entry information to list view
         setContentView(R.layout.activity_main);
         Intent returnIntent = new Intent(this, MainActivity.class);
         returnIntent.putExtra("betName",betName.getText().toString());
-        returnIntent.putExtra("descrip",descripfield.getText().toString());
+        returnIntent.putExtra("descrip",description.getText().toString());
         returnIntent.putExtra("endDate",endDate.getText().toString());
         returnIntent.putExtra("wager",wager.getText().toString());
         returnIntent.putExtra("location",location.getText().toString());
         setResult(Activity.RESULT_OK,returnIntent);
+
         finish();
 
 
 
     }
+
 }
